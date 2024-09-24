7ed32e0a10eef32e8c277f52f60dad0dd68b5f38
Natalya Selyunina <seluninanv@mail.ru>
2024-09-24 21:01:52 +0300

    add validation, create dto classes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -12,22 +14,22 @@ export class MoviesController {
     }
 
     @Get(':id') 
-    getOne(@Param('id') id: string): Movie {
+    getOne(@Param('id') id: number): Movie {
         return this.moviesService.getOne(id);
     }
 
     @Post() 
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(':id') 
-    remove(@Param('id') id: string) {
+    remove(@Param('id') id: number) {
         return this.moviesService.remove(id);
     }
 
     @Patch(':id') 
-    patch(@Param('id') id: string, @Body() updateData) {
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.patch(id, updateData);
     }
 }
